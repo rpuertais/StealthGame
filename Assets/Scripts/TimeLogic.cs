e8e88a705fb130c2cfc2f1f27b7ac4be67c716c4
egutierrezm <egutierrezm@edu.tecnocampus.cat>
2026-02-07 17:23:04 +0100

    time logic
    now it appears the time that you have been in the level

---
+++ Assets/Scripts/TimeLogic.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TimeLogic : MonoBehaviour
+{
+    private Text label;
+    private float totalTime; //guarda el temps total que ha passat des que ha començat la partida
+
+
+    private void Awake()
+    {
+        label = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        totalTime += Time.deltaTime;
+        UpdateTimeText(totalTime);
+    }
+
+    private void UpdateTimeText(float time)
+    {
+        int seconds = Mathf.FloorToInt(time); // passar de float a int
+        label.text = "Time: " + seconds + " sec.";
+    }
+}
