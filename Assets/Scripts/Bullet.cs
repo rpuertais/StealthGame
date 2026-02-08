215f147b478d8d6dca632af024541526f30b5827
jgarciagra <jgarciagra@edu.tecnocampus.cat>
2026-02-08 23:29:58 +0100

    Turret shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float lifeTime = 2f;
+    [SerializeField] private LayerMask hitMask; 
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(Vector2 dir)
+    {
+        dir = dir.normalized;
+        rb.linearVelocity = dir * speed;
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (((1 << other.gameObject.layer) & hitMask) == 0) return;
+
+        
+
+        Destroy(gameObject);
+    }
+}
